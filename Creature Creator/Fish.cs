1efb2b7219016075baeddf5a09539386b4c23901
Richard Norqvist <richard.norqvist@gmail.com>
2018-01-12 13:30:06 +0100

    Started working on a creature creator.

---
+++ Creature Creator/Fish.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Fish", menuName = "Sea Creature/Fish")]
+public class Fish : ScriptableObject {
+
+    public string type;
+    public string description;
+
+}
