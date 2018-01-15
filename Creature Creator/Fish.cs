609897ee87efe12f7a604f83d0ef6a5d6c1d574e
Richard Norqvist <richard.norqvist@gmail.com>
2018-01-15 08:35:10 +0100

    Added data.

--- Creature Creator/Fish.cs
+++ Creature Creator/Fish.cs
@@ -1,11 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Fish", menuName = "Sea Creature/Fish")]
+[CreateAssetMenu(fileName = "New Fish", menuName = "Sea Creatures/Fish")]
 public class Fish : ScriptableObject {
 
-    public string type;
-    public string description;
+    [SerializeField]
+    private string type;
+    [SerializeField]
+    private string description;
+    [SerializeField]
+    private float swimSpeed;
+    [SerializeField]
+    private float turnSpeed;
 
+    public string Type { get { return type; } }
+    public string Description { get { return description; } }
+    public float SwimSpeed { get { return swimSpeed; }  }
+    public float TurnSpeed { get { return turnSpeed; } }
+    
+    public void Print() {
+        Debug.Log("Fish type " + type);
+        Debug.Log("Fish desciption " + description);
+        Debug.Log("Fish speed " + swimSpeed);
+        Debug.Log("Fish turn speed " + turnSpeed);
+    }
+    
 }
